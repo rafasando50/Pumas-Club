95a5dfd10c5eae3bf14962b03c979f2dc5a047ba
rafasando50 <zs21017266@estudiantes.uv.mx>
2025-05-29 14:43:15 -0600

    Primer endpoint implementado

--- src/main/java/api/controller/JugadorController.java
+++ src/main/java/api/controller/JugadorController.java
@@ -1,9 +1,22 @@
 package api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.dto.JugadorDTO;
+import api.service.JugadorService;
 
 @RestController
 public class JugadorController {
     
-    
+    @Autowired
+    private JugadorService jugadorService;
+
+    @RequestMapping("/jugadores")
+    public List<JugadorDTO> getAll() {
+        return jugadorService.getAll();
+    }
 }
